a92335c30a5e2406ef44a0c97677bd3a775e9e20
WiktorWr <wiktor4411@gmail.com>
2024-10-13 19:56:55 +0200

    Configure GH CI

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
-def root():
-    return {"message": "Hello !"}
+def root() -> dict[str, str]:
+    return {"message": f"{add(1,5)}"}
+
+
+def add(a: int, b: int) -> str:
+    return f"result: {a + b}"
