484917e96ce81bef7278265b730c42f0b38d63d6
polymorvic <polymorphic.bite@gmail.com>
2024-11-19 18:15:09 +0100

    fix ruff format issues

--- app/users/router.py
+++ app/users/router.py
@@ -9,6 +9,5 @@ router = APIRouter(prefix="/users", tags=["users"])
 @router.post(
     "/", response_model=UserRepresentation, status_code=status.HTTP_201_CREATED
 )
-
 def create(user: User = Depends(create_user)):
     return user
--- app/users/services.py
+++ app/users/services.py
@@ -5,19 +5,23 @@ from ..database.dependencies import get_db_session
 from .models import User
 from sqlalchemy import select
 
-def create_user(params: CreateUserParams, db_session: Session = Depends(get_db_session)):
-    username = db_session.scalar(select(User).where(User.username==params.username)) # zawara piewsze rekord lub null
+
+def create_user(
+    params: CreateUserParams, db_session: Session = Depends(get_db_session)
+):
+    username = db_session.scalar(
+        select(User).where(User.username == params.username)
+    )  # zawara piewsze rekord lub null
     if username:
-        raise HTTPException(409, 'Username alread taken')
-    
+        raise HTTPException(409, "Username alread taken")
+
     user = User(**params.model_dump())
     db_session.add(user)
     db_session.commit()
     db_session.refresh(user)
     return user
 
-
-    # try:    
+    # try:
     #     user = User(**params.model_dump())
     # except:
     #     pass
